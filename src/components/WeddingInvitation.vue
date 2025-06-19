90064f46afa7d30abea0f82da7ab83e9062de08d
unknown <rizqikurni29@gmail.com>
2025-06-19 18:50:11 +0700

    Coba lagi

--- src/components/WeddingInvitation.vue
+++ src/components/WeddingInvitation.vue
@@ -215,7 +215,7 @@ function scrollToAcara() {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&family=Great+Vibes&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 /* --- Container Utama --- */
-/* .invitation-container {
+.invitation-container {
   max-width: 780px;
   left: 0.2rem; 
   position: relative;
@@ -236,7 +236,7 @@ function scrollToAcara() {
   background:
     linear-gradient(to bottom, #1C1C1C, #1C1C1C, #1C1C1C);
   background-repeat: no-repeat;
-} */
+}
 
 
 .bg-ataskanan {
